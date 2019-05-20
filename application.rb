ee87e18744784b3704438a5641bb0909c696996e
ryanstgermain <ryanjoel26@gmail.com>
2019-05-20 17:40:45 -0600

    made it so you can view individual users with /users/1 or /users/2

--- application.rb
+++ application.rb
@@ -2,6 +2,9 @@ class Application
     def call(env)
         if env["PATH_INFO"] == "/users"
             [200, {}, [Database.users.to_s]]
+        elsif env["PATH_INFO"] =~ %r{/users/\d+}
+            id = env["PATH_INFO"].split("/").last.to_i
+            [200, {}, [Database.users[id].to_s]]
         end
     end
 end
