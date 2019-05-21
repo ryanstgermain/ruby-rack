b19e40abee1d0dabffbd30f0c88f04c73752768f
ryanstgermain <ryanjoel26@gmail.com>
2019-05-21 11:47:41 -0600

    added a response and made the content type to json

--- users_application.rb
+++ users_application.rb
@@ -1,12 +1,19 @@
+require 'json'
+
 class UsersApplication
     def call(env)
+        response = Rack::Response.new
+        response.headers["Content-Type"] = "application/json"
         if env["PATH_INFO"] == ""
-            [200, {}, [Database.users.to_s]]
+            response.write(JSON.generate(Database.users))
         elsif env["PATH_INFO"] =~ %r{/\d+}
             id = env["PATH_INFO"].split("/").last.to_i
-            [200, {}, [Database.users[id].to_s]]
+            response.write(JSON.generate(Database.users[id]))
         else
-            [404, {}, ["Nothing here!"]]
+            response.status = 404
+            response.write("Nothing here!")
         end
+
+        response.finish
     end
 end
